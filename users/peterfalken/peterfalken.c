d247e982d3625909254681daece662af343cbf7e
Peter.Falken <luis@bitjester.com>
2025-04-07 17:43:21 -0600

    chore: reduce size.

--- users/peterfalken/peterfalken.c
+++ users/peterfalken/peterfalken.c
@@ -4,13 +4,17 @@
 #include "peterfalken.h"
 
 // Magic keycodes - overwrite placeholder functions
+#ifndef MAGIC_ENABLE
 uint16_t keycode_config(uint16_t keycode) {
     return keycode;
 }
+#endif
 
+#ifndef MAGIC_ENABLE
 uint8_t mod_config(uint8_t mod) {
     return mod;
 }
+#endif
 
 // Switch layer
 #ifndef CUSTOM_LAYER_STATE
