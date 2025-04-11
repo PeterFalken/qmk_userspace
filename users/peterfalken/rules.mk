8efbc26395bb64f18f4b1f6c1bfa9b3fea401fb6
Peter.Falken <luis@bitjester.com>
2025-04-11 18:50:17 +0000

    chore: refactor default configuration

--- users/peterfalken/rules.mk
+++ users/peterfalken/rules.mk
@@ -9,11 +9,23 @@ GRAVE_ESC_ENABLE = no		# Won't use for now
 MAGIC_ENABLE = no			# Won't use for now
 MOUSEKEY_ENABLE = no		# Won't use for now
 RAW_ENABLE = no				# Disable raw HID commands
-# RGBLIGHT_ENABLE = no		# Disable RGB underglow
 SPACE_CADET_ENABLE = no		# Free up space
 
+# Disable Lighting & RGB features
+RGBLIGHT_ENABLE = no		# Disable RGB underglow
+ifndef RGB_MATRIX_ENABLE
+RGB_MATRIX_ENABLE = no		# Disable keyboard RGB matrix effects
+endif
+
+# Disable encoders by default
+ifndef ENCODER_MAP_ENABLE
+ENCODER_ENABLE = no     	# Disable encoder
+ENCODER_MAP_ENABLE = no		# Disable encoder maps
+endif
+
 # Disable the Audio feature
- MUSIC_ENABLE = no
+MIDI_ENABLE = no
+MUSIC_ENABLE = no
 
 # Enable Link Time Optimizations
 LTO_ENABLE = yes
