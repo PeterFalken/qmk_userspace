b96e4dc2c5e402cc8927b673eecbf207f17c46e1
Peter.Falken <luis@bitjester.com>
2025-04-11 18:48:32 +0000

    chore: 'olkb' configuration clean up for olbk

--- keyboards/preonic/rev3_drop/keymaps/peterfalken/rules.mk
+++ keyboards/preonic/rev3_drop/keymaps/peterfalken/rules.mk
@@ -2,7 +2,3 @@
 # TODO: Check for space optimizations
 AUDIO_ENABLE = yes      # Enable Audio Output
 ENCODER_ENABLE = no     # Disable encoder
-
-ifeq ($(strip $(AUDIO_ENABLE)), yes)
-    SRC += muse.c
-endif
